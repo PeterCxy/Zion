35e8c4bd4858c625f7fa699c702a1ace7d55defb
Peter Cai <peter@typeblog.net>
2020-06-22 14:39:12 +0800

    initial commit

---
+++ metro.config.js
@@ -0,0 +1,23 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+    babelTransformerPath: require.resolve(
+      'react-native-coffeescript-transformer'
+    )
+  },
+  resolver: {
+    sourceExts: ['js', 'coffee']
+  }
+};
---
+++ src/App.coffee
@@ -0,0 +1,7 @@
+import React from "react"
+import { Text } from "react-native"
+
+export default App = () ->
+  <>
+    <Text>Hello, world</Text>
+  </>
